55e7ab55f8e8865d6223b5b5e6054e8af0a10e9c
Rafael Cerqueira <81481716+rafahcerqueira@users.noreply.github.com>
2022-06-20 21:16:38 -0300

    tentativa3

---
+++ codigosJS/indexBotao.js
@@ -0,0 +1,56 @@
+const burguer = document.getElementsByClassName("menu-burguer");  
+
+function toggleMenu(event){     
+
+    if(event.type === 'touchstart') event.preventDefault();
+
+        const cabecalhoMenu = document.getElementById("menu-burguer");
+        nav.classList.toggle('on');     
+        const on = cabecalhoMenu.classList.contains('on');     
+        event.currentTarget.setAttribute('aria-expanded', on); 
+}  
+burguer.addEventListener('click', toggleMenu); 
+burguer.addEventListener('touchstart', toggleMenu);
+
+/*var show = true;
+
+const menuSection = document.querySelector(".menu-burguer");
+const menuBurguer = menuSection.querySelector(".menu-burguer");
+
+menuBurguer.addEventListener("click", () => {
+    menuSection.classList.toggle("on", show)
+    show = !show
+})*/
+
+function redirecionarWpp(){
+    window.open("https://wa.me/5515991097077");
+}
+
+function redirecionarInsta(){
+    window.open("https://www.instagram.com/rafaael1/");
+}
+
+function redirecionarEmail(){
+    window.open("https://accounts.google.com/AccountChooser/signinchooser?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser");
+}
+
+function idadeMostrar(){
+    let idade = document.getElementById("idade").value;
+    document.getElementById("idade1").innerHTML = Number(idade);
+}
+
+function pesoMostrar(){
+    let peso = document.getElementById("peso").value;
+    document.getElementById("peso1").innerHTML = Number(peso);
+}
+
+function alturaMostrar(){
+    let altura = document.getElementById("altura").value;
+    document.getElementById("altura1").innerHTML = Number(altura);
+}
+
+function enviar(){
+    let nome1 = document.getElementById("nome").value;
+
+    document.getElementById("result").innerHTML = "Olá " + nome1 + "," + " seu cadastro foi realizado com sucesso!";
+}
